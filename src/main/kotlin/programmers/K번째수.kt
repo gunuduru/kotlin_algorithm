017eba9bf019796d54694dad37f380cf91b56228
gunuduru <goonoo0731@gmail.com>
2025-01-02 23:11:07 +0900

    [프로그래머스] K번째 수
    - 프로그래머스 기준 kotlin 버전이 1.6.10이라, ..< 사용이 불가능하여 until로 대체하였음

--- src/main/kotlin/programmers/K번째수.kt
+++ src/main/kotlin/programmers/K번째수.kt
@@ -1,9 +1,25 @@
 package main.kotlin.programmers
 
 class Programmers42748 {
+    companion object {
+        // array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3
+        // -> 2번째~5번째 자르면 [5,2,6,3] -> 정렬하면 [2,3,5,6] -> k(3)번째 숫자는 5
+        fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
+            val result = mutableListOf<Int>()
+            commands.forEach { command ->
+                val sliced = array.sliceArray(command[0] - 1 until command[1]).sorted()
+                result.add(sliced[command[2] - 1])
+            }
+
+            return result.toIntArray()
+        }
+    }
 
 }
 
 fun main() {
-    println("Hello World")
+    val array: IntArray = intArrayOf(1, 5, 2, 6, 3, 7, 4)
+    val commands: Array<IntArray> = arrayOf(intArrayOf(2, 5, 3), intArrayOf(4, 4, 1), intArrayOf(1, 7, 3))
+    println(Programmers42748.solution(array, commands).contentToString())
 }
+
