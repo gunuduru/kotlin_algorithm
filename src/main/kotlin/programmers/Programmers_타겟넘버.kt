93eed0862b767ba210e9e6ddd58249842fccfa85
gunuduru <goonoo0731@gmail.com>
2025-01-03 14:21:23 +0900

    [Programmers] 타겟 넘버
    * visited를 사용하지 않아도 되는 dfs문제인데, visited를 사용하다가 시간을 낭비했다.
       * 순차적으로 사용해야 하는지, 순서가 섞여도 되는지를 문제확인 단계에서 확실하게 판단하자.

--- src/main/kotlin/programmers/Programmers_타겟넘버.kt
+++ src/main/kotlin/programmers/Programmers_타겟넘버.kt
@@ -2,15 +2,39 @@ package main.kotlin.programmers
 
 class Programmers_타겟넘버 {
     companion object {
+        var numberList = mutableListOf<Int>()
+        var cnt = 0
+        var targetNum = 0
+        var totalDepth = 0
+
         fun solution(numbers: IntArray, target: Int): Int {
-            var answer = 0
-            return answer
+            targetNum = target
+            totalDepth = numbers.size
+            numberList = numbers.toMutableList()
+            dfs(0, 0)
+
+            return cnt
+        }
+
+        /**
+         * @param depth 현재 누적된 depth
+         */
+        private fun dfs(curSum: Int, depth: Int) {
+            if (depth == totalDepth) {
+                if (curSum == targetNum) {
+                    cnt++
+                }
+            } else {
+                // +로 한번, -로 한번
+                dfs(curSum + numberList[depth], depth + 1)
+                dfs(curSum - numberList[depth], depth + 1)
+            }
         }
     }
 }
 
 fun main() {
-    val numbers = intArrayOf(1,1,1,1,1)
+    val numbers = intArrayOf(1, 1, 1, 1, 1)
     val target = 3
 
     println(Programmers_타겟넘버.solution(numbers, target))
