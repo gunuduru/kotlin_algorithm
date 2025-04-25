0f420ac051ae84e2afe1ec44fdbbf3e77d05b67f
gunuduru <goonoo0731@gmail.com>
2025-04-26 00:42:30 +0900

    [프로그래머스] 최소직사각형
    - 아! 간단하다!
    - 회전이 가능하므로, 애초에 가로/세로를 큰거/작은거 로 두면 간단해지는 거였음
       - 알고보면 간단한 문제인데 복잡하게 푸는 실수가 많다.. 반성하자. 공부하자.

--- src/main/kotlin/programmers/Programmers_최소직사각형.kt
+++ src/main/kotlin/programmers/Programmers_최소직사각형.kt
@@ -1,51 +1,22 @@
 package main.kotlin.programmers
 
-import java.util.*
+import main.kotlin.programmers.Programmers_최소직사각형.Companion.solution
 import kotlin.math.max
+import kotlin.math.min
 
 class Programmers_최소직사각형 {
     companion object {
-        data class Square(val width: Int, val height: Int) {
-            /** 90도 회전시 기존 최대범위 이내에 포함되는가 */
-            fun isIncludedWhenRotate(maxWidth: Int, maxHeight: Int): Boolean {
-                return height <= maxWidth && width <= maxHeight
-            }
-        }
-
         fun solution(sizes: Array<IntArray>): Int {
-            val squareList = sizes.map { size -> Square(width = size[0], height = size[1]) }
-
-            // 1. 전체 사각형의 가로/세로 길이 최대값을 구함
             var maxWidth = 0
             var maxHeight = 0
 
             sizes.forEach { size ->
-                maxWidth = max(maxWidth, size[0])
-                maxHeight = max(maxHeight, size[1])
-            }
+                // 긴 것을 width로, 짧은 것을 height로 둔다 (왜냐면 회전 가능하므로)
+                val w = max(size[0], size[1])
+                val h = min(size[0], size[1])
 
-            // 2. 현재 (가로 또는 세로가) 최대값을 갖는 명함을 회전했을 때, 기존 최대범위 이내에 존재한다면 해당 (가로 or 세로)값은 축소가능하다고 본다.
-
-            // 2-1. 가로 쳌
-            squareList.sortedWith(Comparator.comparing<Square, Int> { it.width }.thenBy { it.height })
-            for (square in squareList) {
-                if (square.width < maxWidth) maxWidth = square.width // 이전 반복문에서 최대값 사각형 회전시 포함되어서 continue된 상태
-                if (square.isIncludedWhenRotate(maxWidth, maxHeight)) {
-                    continue // 최대값 사각형 회전시 포함되므로 다음 사각형 확인
-                } else {
-                    break // 회전시 포함 안된다면 더이상 탐색 안함
-                }
-            }
-
-            // 2-2. 세로 쳌
-            squareList.sortedWith(Comparator.comparing<Square, Int> { it.height }.thenBy { it.width })
-            for (square in squareList) {
-                if (square.height < maxHeight) maxHeight = square.height // 이전 반복문에서 최대값 사각형 회전시 포함되어서 continue된 상태
-                if (square.isIncludedWhenRotate(maxWidth, maxHeight)) {
-                    continue // 최대값 사각형 회전시 포함되므로 다음 사각형 확인
-                } else {
-                    break // 회전시 포함 안된다면 더이상 탐색 안함
-                }
+                maxWidth = max(maxWidth, w)
+                maxHeight = max(maxHeight, h)
             }
 
             return maxWidth * maxHeight
@@ -54,10 +25,7 @@ class Programmers_최소직사각형 {
 }
 
 fun main() {
-//    val priorities = intArrayOf(2, 1, 3, 2)
-    val priorities = intArrayOf(1, 1, 9, 1, 1, 1)
-//    val location = 2
-    val location = 0
+    val sizes = arrayOf(intArrayOf(60, 50), intArrayOf(30, 70), intArrayOf(60, 30), intArrayOf(80, 40))
 
-    println(Programmers_프로세스.solution(priorities, location))
+    println(solution(sizes))
 }
