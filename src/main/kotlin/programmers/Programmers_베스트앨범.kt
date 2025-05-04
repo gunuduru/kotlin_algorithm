8a690f280cf83c522c4677a091ed6b093304e88a
gunuduru <goonoo0731@gmail.com>
2025-05-04 22:23:00 +0900

    [프로그래머스] [코드 개선] 베스트앨범
    - 단순 정렬만 필요한 pq를 list로 수정
       - sortedByDescending으로 정렬 해결
    - repeat 내부에서 coerceAtMost 쓰던걸 minOf로 간소화
       - take(2)를 사용해도 된다
    - 변수명 수정

--- src/main/kotlin/programmers/Programmers_베스트앨범.kt
+++ src/main/kotlin/programmers/Programmers_베스트앨범.kt
@@ -6,36 +6,30 @@ import java.util.*
 class Programmers_베스트앨범 {
     companion object {
         data class PlayIndex(val play: Int, val idx: Int)
-        data class GenrePlayCnt(val genre: String, val playCnt: Int)
 
         fun solution(genres: Array<String>, plays: IntArray): IntArray {
-            val genreToPlayIndexPQ = mutableMapOf<String, PriorityQueue<PlayIndex>>()
+            val genreMap = mutableMapOf<String, PriorityQueue<PlayIndex>>()
 
-            val result = mutableListOf<Int>()
-
-            for (i in 0 until genres.size) {
-                genreToPlayIndexPQ.computeIfAbsent(genres[i]) {
-                    PriorityQueue<PlayIndex>(
-                        compareByDescending<PlayIndex> { it.play }.thenBy { it.idx }
-                    )
+            // 1. 장르별 우선순위 큐 구성
+            for (i in genres.indices) {
+                val queue = genreMap.getOrPut(genres[i]) {
+                    PriorityQueue(compareByDescending<PlayIndex> { it.play }.thenBy { it.idx })
                 }
-
-                genreToPlayIndexPQ[genres[i]]!!.add(PlayIndex(plays[i], i))
+                queue.add(PlayIndex(plays[i], i))
             }
 
-            val pq = PriorityQueue<GenrePlayCnt>(compareByDescending { it.playCnt })
+            // 2. 장르별 총 재생수 기준 정렬
+            val sortedGenres = genreMap.entries
+                .map { it.key to it.value.sumOf { pi -> pi.play } }
+                .sortedByDescending { it.second }
 
-            genreToPlayIndexPQ.entries.map { pq.add(GenrePlayCnt(it.key, it.value.sumOf { it.play })) }
-
-            while (pq.isNotEmpty()) {
-                val genre = pq.poll().genre
-
-                // 2 이하의 숫자로 반환
-                repeat(genreToPlayIndexPQ[genre]!!.size.coerceAtMost(2)) {
-                    val cur = genreToPlayIndexPQ[genre]!!.poll()
-                    result.add(cur.idx)
+            // 3. 각 장르별 상위 2개 곡 추출
+            val result = mutableListOf<Int>()
+            for ((genre, _) in sortedGenres) {
+                val queue = genreMap[genre]!!
+                repeat(minOf(2, queue.size)) {
+                    result.add(queue.poll().idx)
                 }
-
             }
 
             return result.toIntArray()
